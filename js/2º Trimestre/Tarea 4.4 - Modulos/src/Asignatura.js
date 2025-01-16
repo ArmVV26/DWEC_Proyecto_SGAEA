8a3b34df591333ff37024c056b5858d34833a040
ArmVV26 <ditovaquero@gmail.com>
2025-01-17 00:07:42 +0100

    Tarea 4.4

---
+++ js/2º Trimestre/Tarea 4.4 - Modulos/src/Asignatura.js
@@ -0,0 +1,208 @@
+/**
+ * @class Asignatura
+ * Clase que crea las asignaturas y contiene las calificaciones de los estudiantes.
+ * > ---
+ * > ### Atributos:
+ * > - `#nombre`: Nombre para las asignaturas. [String]
+ * > - `#calificaciones`: Mapa que contendrá el ID el Estudiante y sus calificaciones. [Object]
+ * >
+ * > ### Constructor:
+ * > - Lo primero que hago es verificar si el nombre es válido (solo letras y espacios).
+ * >     + Si es válido, le asigno el nombre al atributo "this.#nombre" y el atributo "this.#calificacion" lo inicializo a un mapa vacío.
+ * >     + Si no es válido, mostrará un error indicando que el nombre no es válido.
+ * >
+ * > ### Getter:
+ * > - Declaro el getter de nombre, que me devuelve el nombre de la Asignatura.
+ * >
+ * > ### Métodos:
+ * > - `obtenerCalificaciones(id_Estudiante)`: Devuelvo un array con las calificaciones del Estudiante que paso por parámetros, de una asignatura determinada.
+ * > - `addCalificacion(id_Estudiante, calificacion)`: Añade una nota al Estudiante en una asignatura determinada. Pide por parámetros el id_Estudiante y la calificación a añadir.
+ * >     + 1º - Comprueba que la calificación es correcta (un número y, >0 y <10). Si no es correcta, lanza un error.
+ * >     + 2º - Comprueba si "this.#calificaciones" ya tiene registrado el ID del Estudiante.
+ * >         - Si tiene dentro ya una Key que es la del Id del Estudiante, le añade otra calificación.
+ * >         - Si no, añade de un nuevo id_Estudiante y una calificación a "this.#calificacion".
+ * >> ( Compruebo con "mapa.has.(key)", añado calificación con "mapa.get(key).push(valor)" y añado uno nuevo con "mapa.set(key, [valor])").
+ * > - `calcularPromedioGeneral()`: Calcula el promedio de las calificaciones que hay en una Asignatura. Para hacerlo hago lo siguiente:
+ * >     + 1º - Declaro dos variables:
+ * >         - "total_Notas": contendrá la suma total de todas las notas. [Number]
+ * >         - "cantidad_Notas":  contendrá el número total de notas. [Number]
+ * >     + 2º - Luego recorro "this.#calificaciones" (usando un .forEach).
+ * >     + 3º - Dentro de este creo:
+ * >         - "suma_Notas": variable que sumará todas las notas (usando .reduce).
+ * >         - "total_Notas": sumará todas las suma_Notas con la iteración del bucle.
+ * >         - "contidad_Notas": sumará la cantidad de notas que tiene cada Estudiante.
+ * >    + 4º - Compruebo si hay alguna calificación (total_Notas > 0):
+ * >        - Si no, devuelvo "Sin calificaciones".
+ * >       - Si tenia, devuelvo el cálculo del promedio (total_Notas / cantidad_Notas), con dos decimales (usando .toFixed(2) -> [String]).
+ * > - `calcularPromedioEstudiante(id_Estudiante)`: Calcula el promedio de las calificaciones de un Estudiante en una Asignatura. Para hacerlo hago lo siguiente:
+ * >     + 1º - Declaro:
+ * >         - "notas": usa el método "obtenerCalificaciones(id_Estudiante)" para obtener las calificaciones.
+ * >         - "total_Notas": sumará todas las notas.
+ * >         - "cantidad_Notas": obtendrá la cantidad de notas.
+ * >    + 2º - Y por último, devuelvo lo mismo que en el método anterior.
+ * > - `mostrarAsignaturas()`: Sirve para mostrar todas las Asignaturas de una forma formateada.
+ * > ---
+ */
+export class Asignatura{
+
+    // Atributos privados
+    #nombre;
+    #calificaciones;
+
+    /**
+     * Constructor de la clase Asignatura que crea una instancia de esta.
+     * 
+     * @param {string} nombre - Nombre de la Asignatura.
+     * @throws {Error} Si el nombre no esta formado solo por letras y espacios.
+     * 
+     * @example
+     * // Crear una instancia de Asignatura.
+     * const asignatura = new Asignatura("DAWES");
+     */
+    constructor(nombre) {
+
+        if ((/^[a-zA-ZáéíóúÁÉÍÓÚ ]+$/).test(nombre)) {
+            this.#nombre = nombre;
+            this.#calificaciones = new Map();
+        }else{
+            throw new Error("El nombre de la Asignatura es erroneo");
+        }
+    }
+
+    /**
+     * Obtiene el nombre de la Asignatura.
+     * 
+     * @returns {string} Nombre de la Asignatura.
+     */
+    get nombre() {
+        return this.#nombre;
+    }
+    
+    // Metodos de la clase Asignatura.
+    /**
+     * @function
+     * @description Obtener calificaciones de un Estudiante en una Asignatura.
+     * 
+     * @param {string} id_Estudiante - ID del Estudiante.
+     * @returns {number[]} Array con las calificaciones del Estudiante en una Asignatura.
+     * 
+     * @example
+     * // Devuelve un array con las calificaciones del Estudiante.
+     * // Por ejemplo: [5, 10, 7, 8]
+     */
+    obtenerCalificaciones(id_Estudiante) {
+        return this.#calificaciones.get(id_Estudiante) || [];
+    }
+    
+    /**
+     * @function
+     * @description Añade una calificación para un Estudiante en una Asignatura.
+     * 
+     * @param {string} id_Estudiante - ID del Estudiante.
+     * @param {number} calificacion - Calificación a añadir.
+     * 
+     * @throws {Error} Si la calificación no es un número o es NaN.
+     * @throws {Error} Si la calificación no es válida (0 a 10).
+     */
+    addCalificacion(id_Estudiante, calificacion) {
+
+        if (typeof calificacion !== "number" || isNaN(calificacion)) {
+            throw new Error(`La calificación asignada a la asignatura ${this.nombre} no es un numero`);
+        }
+
+        if (calificacion < 0 || calificacion > 10) {
+            throw new Error(`La calificacón asignada a la asignatura ${this.#nombre} no es valida (0 a 10)`);
+        }
+         
+        if (this.#calificaciones.has(id_Estudiante)) {
+
+            this.#calificaciones.get(id_Estudiante).push(calificacion);
+        }else{
+
+            this.#calificaciones.set(id_Estudiante, [calificacion]);
+        }
+    }
+
+    /**
+     * @function
+     * @description Calcula el promedio general de las calificaciones totales, de una Asignatura.
+     * 
+     * @returns {string | number} Promedio General o sin calificaciones.
+     * 
+     * @example
+     * // Devuelve el promedio general de las calificaciones o, en su defecto, "sin calificaciones" .
+     * // Por ejemplo: 7.50
+     */
+    calcularPromedioGeneral() {
+
+        let total_Notas = 0;
+        let cantidad_Notas = 0;
+    
+        this.#calificaciones.forEach((notas, id) => {
+
+            let suma_Notas = 0;
+            suma_Notas = notas.reduce((total, valorAct) => total + valorAct, 0);
+            total_Notas += suma_Notas;
+
+            cantidad_Notas += notas.length;
+        });
+
+        return total_Notas > 0 ? (total_Notas / cantidad_Notas).toFixed(2) : "Sin calificaciones"; 
+    }
+
+    /**
+     * @function
+     * @description Calcula el promedio de las calificaciones de un Estudiante en una Asignatura.
+     * 
+     * @param {string} id_Estudiante - ID del Estudiante. 
+     * 
+     * @returns {string|number} Promedio del Estudiante en una Asignatura.
+     * 
+     * @example
+     * // Devuelve el promedio de las calificaciones de un Estudiante en una Asignatura o, en su defecto, "sin evaluar".
+     * // Por ejemplo: 7.50
+     */
+    calcularPromedioEstudiante(id_Estudiante) {
+
+        const notas = this.obtenerCalificaciones(id_Estudiante);
+        const total_Notas = notas.reduce((total, valorAct) => total + valorAct, 0);
+        const cantidad_Notas = notas.length; 
+
+        return total_Notas > 0 ? (total_Notas / cantidad_Notas).toFixed(2) : "Sin evaluar";
+    }
+
+    /**
+     * @function
+     * @description Muestra una representación de los datos formateados de las Asignaturas y sus calificaciones.
+     * 
+     * @returns {string} Cadena con los datos de las Asignaturas formateados.
+     * 
+     * @example
+     * // Devuelve una cadena con los datos de las Asignaturas formateados.
+     * // Por ejemplo:
+     * // ───────────────────────────────
+     * //            DAWES
+     * // ===============================
+     * // - DAWES: 5 - 10 - 7 - 8
+     * // ------------------------------
+     * //   - Promedio = 7.50
+     */
+    mostrarAsignaturas() {
+        let mostrar_Asig = "─────────────────────────────── \n" +
+                           `            ${this.#nombre} \n` +
+                           "=============================== \n";
+        if (this.#calificaciones.size === 0) {
+            mostrar_Asig += "Sin calificaciones \n" +
+                            "============================== \n";  
+        }else {
+            this.#calificaciones.forEach((notas, id) => {
+                mostrar_Asig += `${id}: ${notas.join(" - ")} \n`;
+            });
+            mostrar_Asig += "------------------------------ \n" +
+                            `  - Promedio = ${this.calcularPromedioGeneral()} \n` +
+                            "────────────────────────────── \n";   
+        }
+
+        return mostrar_Asig;
+    }
+}
---
+++ js/2º Trimestre/Tarea 4.4 - Modulos/src/Lista.js
@@ -0,0 +1,224 @@
+/**
+ * @class Lista
+ * Clase que gestiona los Estudiantes y Asginaturas, permitiendo agregar, eliminar, listar o buscar los elementos que contienen estas listas.
+ * 
+ * > ---
+ * > ### Atributos:
+ * > - `#estudiantes`: Lista que contendrá todos los Estudiantes. [Array<Estudiante>]
+ * > - `#asignaturas`: Lista que contendrá todas las Asignaturas. [Array<Asignatura>]
+ * >
+ * > ### Constructor:
+ * > - Lo que hago es inicializar las listas como arrays vacíos.
+ * >
+ * > ### Getters:
+ * > - Declaro dos métodos Getters para las listas estudiantes y asignaturas.
+ * >
+ * > ### Métodos:
+ * > - `agregarEstudiante(alumno)`: Añade un Estudiante a "this.#estudiantes". Funcionalidad:
+ * >     + Compruebo si el Estudiante ya se encuentra dentro.
+ * >     + Si está dentro, devuelvo un mensaje indicándolo.
+ * >     + Si no, lo añado y muestro un mensaje indicando que lo he añadido.
+ * > - `agregarAsignatura(materia)`: Añade una Asignatura a "this.#asignaturas". Su funcionalidad es la misma que el anterior método, pero con Asignatura.
+ * > - `eliminarEstudiante(alumno)`: Elimina un Estudiante de "this.#estudiantes". Funcionalidad:
+ * >     + Busco la posición en "this.#estudiantes" del alumno que paso por parámetros.
+ * >     + Si la posición es !== -1, el Estudiante está en la lista. Se elimina el alumno de "this.#estudiantes" y se guarda el número del ID en la variable constante de la clase Estudiante, "id_Disponibles".
+ * >     + Si es eliminado, mostrarar un mensaje indicándolo, y si no es eliminado, se indicara con un mensaje también.
+ * > - `eliminarAsignatura(materia)`: Elimina una Asignatura de "this.#asignaturas". Su funcionalidad es la misma que el anterior método, pero con Asignatura. No añade nada a "id_Disponibles".
+ * > - `buscarEstudiante(patron)`: Busca Estudiantes en "this.#estudiantes" por un patrón de su nombre. Funcionalidad:
+ * >     + Con el método ".filter", recorro todos los alumnos que están registrados en "this.#estudiantes" y con su nombre en minúscula, compruebo si contiene el patrón que paso por parámetros en el nombre. Si lo contiene, este alumno se guarda en la variable "resultado".
+ * >     + Compruebo que la variable "resultado" no esta vacía:
+ * >         - Si no lo está, recorre "resultado" y ejecuta para cada instacia el método de la clase Estudiante, "mostrarEstudiante()".
+ * >         - Si lo está, muestra un mensaje indicando que no existen Estudiantes registrados con ese patrón.
+ * > - `buscarAsignatura(patron)`: Busca Asignaturas en "this.#asignaturas" por un patrón de su nombre. Su funcionalidad es la misma que la del anterior método, pero con Asignatura.
+ * > ---
+ */
+export class Lista {
+
+    // Atributos privados
+    #estudiantes;
+    #asignaturas;
+
+    /**
+     * Constructor de la clase Lista.
+     * 
+     * @example
+     * // Crear una instancia de Lista.
+     * const lista = new Lista();
+     */
+    constructor() {
+        this.#estudiantes = [];
+        this.#asignaturas = [];
+    }
+
+    /**
+     * Obtiene la lista de Estudiantes.
+     * 
+     * @returns {Estudiante[]} Lista de Estudiantes.
+     */
+    get estudiantes() {
+        return this.#estudiantes;
+    }
+    /**
+     * Obtiene la lista de Asignaturas.
+     * 
+     * @returns {Asignatura[]} Lista de Asignaturas.
+     */
+    get asignaturas() {
+        return this.#asignaturas;
+    }
+
+    // Metodos de la clase Lista
+    /**
+     * @function
+     * @description Agrega un Estudiante a "this.#estudiantes".
+     * 
+     * @param {Estudiante} alumno - Instancia de la clase Estudiante.
+     * @returns {string} Mensaje que indica si el Estudiante a sido añadido o no.
+     * 
+     * @example
+     * // Añadir un Estudiante a la lista.
+     * // Por ejemplo: "El estudiante ha sido añadido"
+     */
+    agregarEstudiante(alumno) {
+        if (this.#estudiantes.includes(alumno)) {
+            return "El estudiante ya está en la lista";
+        }else {
+            this.#estudiantes.push(alumno);
+            return "El estudiante ha sido añadido";
+        }
+    }
+
+    /**
+     * @function
+     * @description Agrega una Asignatura a "this.#asignaturas".
+     * 
+     * @param {Asignatura} materia - Instancia de la clase Asignatura. 
+     * @returns {string} Mensaje que indica si la Asignatura a sido añadido o no.
+     * 
+     * @example
+     * // Añadir una Asignatura a la lista.
+     * // Por ejemplo: "La asignatura ha sido añadida"
+     */
+    agregarAsignatura(materia) {
+        if (this.#asignaturas.includes(materia)) {
+            return "La asignatura ya está en la lista";
+        }else {
+            this.#asignaturas.push(materia);
+            return "La asignatura ha sido añadida";
+        }
+    }
+
+    /**
+     * @function
+     * @description Elimina un Estudiante a "this.#estudiantes".
+     * 
+     * @param {Estudiante} alumno - Instancia de la clase Estudiante. 
+     * @returns {string} Mensaje que indica si el Estudiante a sido eliminado o no.
+     * 
+     * @example
+     * // Eliminar un Estudiante de la lista.
+     * // Por ejemplo: "Estudiante eliminado
+     */
+    eliminarEstudiante(alumno) {
+        const posicion = this.#estudiantes.indexOf(alumno);
+        if (posicion !== -1) {
+            this.#estudiantes.splice(posicion, 1);
+
+            Estudiante.id_Disponibles.push(parseInt(alumno.id.substring(3))); // Guardo el ID en id_Disponibles
+
+            return "Estudiante eliminado";
+        }else {
+            return "El estudiante no esta en la lista";
+        }
+    }
+
+    /**
+     * @function
+     * @description Elimina una Asignatura a "this.#asignaturas".
+     * 
+     * @param {Asignatura} materia - Instancia de la clase Asignatura. 
+     * @returns {string} Mensaje que indica si la Asignatura a sido eliminado o no.
+     * 
+     * @example
+     * // Eliminar una Asignatura de la lista.
+     * // Por ejemplo: "Asignatura eliminada"
+     */
+    eliminarAsignatura(materia) {
+        const posicion = this.#asignaturas.indexOf(materia);
+        if (posicion !== -1) {
+            this.#asignaturas.splice(posicion, 1);
+            return "Asignatura eliminada";
+        }else {
+            return "La asignatura no esta en la lista";
+        }
+    }
+
+    /**
+     * @function
+     * @description Busca Estudiantes en "this.#estudiantes" por un patrón de su nombre.
+     * 
+     * @param {string} patron - Cadena a buscar en los nombres de los Estudiantes. 
+     * @returns {string} Cadena formateada de los Estudiantes o mensaje indicando que no sean encotrado.
+     * 
+     * @example
+     * // Buscar un Estudiante en la lista.
+     * // Por ejemplo:
+     * // ────────────────────────────────────
+     * //     ARM001 - Armando Vaquero
+     * // ===================================
+     * // Edad: 23
+     * // Direccion:
+     * //    - Calle      : Avenida Andalucia
+     * //    - Numero     : 45
+     * //    - Piso       : 3
+     * //    - Cod. Postal: 18010
+     * //    - Provincia  : Granada
+     * //    - Localidad  : Granada
+     * // ===================================
+     * //    Asignaturas y Calificaciones
+     * // ===================================
+     * // DAWES: 5 - 10 - 7 - 8 | Promedio: 7.50
+     * // -----------------------------------
+     * // Promedio Total: 7.50 
+     */
+    buscarEstudiante(patron) {
+        const resultado = this.#estudiantes.filter(alumno => 
+            alumno.nombre.toLowerCase().includes(patron.toLowerCase())
+        );
+
+        if (resultado.length > 0) {
+            return resultado.map(alumno => alumno.mostrarEstudiante()).join("\n");
+        }else {
+            return "No se ha encontrado estudiantes con ese patron";
+        }
+    }
+
+    /**
+     * @function
+     * @description Busca Asignaturas en "this.#asignaturas" por un patrón de su nombre.
+     * 
+     * @param {string} patron - Cadena a buscar en los nombres de las Asignaturas. 
+     * @returns {string} Cadena formateada de las Asignaturas o mensaje indicando que no sean encotrado.
+     * 
+     * @example
+     * // Buscar una Asignatura en la lista.
+     * // Por ejemplo:
+     * // ───────────────────────────────
+     * //            DAWES
+     * // ===============================
+     * // - DAWES: 5 - 10 - 7 - 8
+     * // ------------------------------
+     * //   - Promedio = 7.50
+     */
+    buscarAsignatura(patron) {
+        const resultado = this.#asignaturas.filter(materia => 
+            materia.nombre.toLowerCase().includes(patron.toLowerCase())
+        );
+
+        if (resultado.length > 0) {
+            return resultado.map(materia => materia.mostrarAsignaturas()).join("\n");
+        }else {
+            return "No se ha encontrado materias con ese patron";
+        }
+    }
+}
