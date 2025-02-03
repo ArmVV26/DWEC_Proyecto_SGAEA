6c0d0da46284212e1e02a2595399bdd783745ff8
Armando Vaquero Vargas <ditovaquero@gmail.com>
2025-02-03 16:52:12 +0100

    Cambio Version Nav

--- js/2º Trimestre/Tarea 4.5 - Polyfill y Transpile/babel.config.js
+++ js/2º Trimestre/Tarea 4.5 - Polyfill y Transpile/babel.config.js
@@ -3,13 +3,7 @@ export default {
         [
             "@babel/preset-env",
             {
-                // Esto es para la version de navegadores que queremos que soporte.
-                targets: {
-                   chrome: "63",
-                   firefox: "60",
-                   safari: "12",
-                   ie: "11" 
-                },
+                targets: "> 0.25%, not dead", // Esto es para la version de navegadores que queremos que soporte.
                 useBuiltIns: "usage", // Incluya solo los polyfills que necesitamos.
                 corejs: 3 // Version de corejs que estamos usando.
             }
